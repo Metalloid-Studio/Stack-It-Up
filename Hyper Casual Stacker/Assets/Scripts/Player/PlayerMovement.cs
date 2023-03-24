c5bceb37c9aa775e3217706d183fd9c01f1c92cd
ElyasTH <elyastashakkori@gmail.com>
2023-03-24 15:48:15 +0430

    Fixed player Y axis movement

--- Hyper Casual Stacker/Assets/Scripts/Player/PlayerMovement.cs
+++ Hyper Casual Stacker/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,21 +8,21 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private float speed;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        print(Input.mousePosition);
+        // print(Input.mousePosition);
         if (Input.GetMouseButton(0)){
+
             Vector3 moveDirection = new Vector3(Input.mousePosition.x - Screen.width/2, 0, Input.mousePosition.y - Screen.height/2).normalized;
-            rb.velocity = moveDirection * speed * Time.deltaTime * 100;
-            print(moveDirection);
+            float deltaSpeed = speed * Time.deltaTime * 100;
+
+            rb.velocity = new Vector3(moveDirection.x*deltaSpeed, rb.velocity.y, moveDirection.z*deltaSpeed);
         }
-        else rb.velocity = Vector3.zero;
+        else rb.velocity = new Vector3(0,rb.velocity.y,0);
     }
 }
