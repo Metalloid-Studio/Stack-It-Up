cebcc79e56032bcde8d1c7a92150bba354bf9b50
ElyasTH <elyastashakkori@gmail.com>
2023-03-24 15:29:15 +0430

    updated picker script

--- Hyper Casual Stacker/Assets/Scripts/Characters/Picker.cs
+++ Hyper Casual Stacker/Assets/Scripts/Characters/Picker.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Picker : MonoBehaviour
 {
-    private void Pick(Pickable pickable){
-        pickable.GetPicked(this);
+    [SerializeField]
+    private List<Pickable> inventory;
+
+    void Start(){
+        inventory = new List<Pickable>();
+    }
+
+    private void Pick(Pickable newObject){
+        newObject.GetPicked(this);
+        inventory.Add(newObject);
+    }
+
+    private void Throw(Pickable objectToRemove){
+        
+        inventory.Remove(objectToRemove);
+    }
+
+    public void ThrowAll(){
+
+        inventory.Clear();
     }
 
     void OnCollisionEnter(Collision col){
